3d437b00f16f3b1578e6fb664d43e8a029e2dbb1
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-07-04 19:17:09 -0300

    feat: alterando o codifo para ler uma matriz e nao apenas receber

--- ex1.js
+++ ex1.js
@@ -1,17 +1,42 @@
-var A = [[2, -1], [2, 0]];
+var A = [];
+var linhasA = parseInt(prompt("Digite o número de linhas da matriz A:"));
+var colunasA = parseInt(prompt("Digite o número de colunas da matriz A:"));
 
-var B = [[2, 3], [-2, 1]];
-
-var resultado = [[0, 0], [0, 0]];
-
-for (var i = 0; i < 2; i++) {
-    for (var j = 0; j < 2; j++) {
-        for (var k = 0; k < 2; k++) {
-            resultado[i][j] += A[i][k] * B[k][j];
-        }
-    }
+for (var i = 0; i < linhasA; i++) {
+    A[i] = [];
+    for (var j = 0; j < colunasA; j++) 
+        {   A[i][j] = parseInt(prompt(`Digite o elemento A[${i+1}][${j+1}]:`));  }
 }
 
-for (var i = 0; i < 2; i++) {
-    console.log(resultado[i]);
+var B = [];
+var linhasB = parseInt(prompt("Digite o número de linhas da matriz B:"));
+var colunasB = parseInt(prompt("Digite o número de colunas da matriz B:"));
+
+for (var i = 0; i < linhasB; i++) {
+    B[i] = [];
+    for (var j = 0; j < colunasB; j++) 
+        {   B[i][j] = parseInt(prompt(`Digite o elemento B[${i+1}][${j+1}]:`));   }
+}
+
+if (colunasA !== linhasB) 
+    {  console.log("Impossível fazer a multiplicação");  } 
+else {
+    var resultado = [];
+    for (var i = 0; i < linhasA; i++) {
+        resultado[i] = [];
+        for (var j = 0; j < colunasB; j++) {
+            resultado[i][j] = 0;
+        }
+    }
+
+    for (var i = 0; i < linhasA; i++) {
+        for (var j = 0; j < colunasB; j++) {
+            for (var k = 0; k < colunasA; k++) {
+                resultado[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    for (var i = 0; i < resultado.length; i++) 
+        {   console.log(resultado[i]);   }
 }
