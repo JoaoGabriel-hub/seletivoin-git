a503c238c0efc6106910d6e0298d2e3dfe4f66ee
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-07-04 19:23:32 -0300

    feat: adicionando comentarios ao codigo

--- ex1.js
+++ ex1.js
@@ -1,17 +1,21 @@
+// criando um array e lendo as dimensões de A
 var A = [];
 var linhasA = parseInt(prompt("Digite o número de linhas da matriz A:"));
 var colunasA = parseInt(prompt("Digite o número de colunas da matriz A:"));
 
+// lendo os elementos de A
 for (var i = 0; i < linhasA; i++) {
     A[i] = [];
     for (var j = 0; j < colunasA; j++) 
         {   A[i][j] = parseInt(prompt(`Digite o elemento A[${i+1}][${j+1}]:`));  }
 }
 
+// criando um array e lendo as dimensões de B
 var B = [];
 var linhasB = parseInt(prompt("Digite o número de linhas da matriz B:"));
 var colunasB = parseInt(prompt("Digite o número de colunas da matriz B:"));
 
+// lendo os elementos de B
 for (var i = 0; i < linhasB; i++) {
     B[i] = [];
     for (var j = 0; j < colunasB; j++) 
@@ -21,6 +25,7 @@ for (var i = 0; i < linhasB; i++) {
 if (colunasA !== linhasB) 
     {  console.log("Impossível fazer a multiplicação");  } 
 else {
+    // criando uma matriz resultado com 0 em todos os elementos
     var resultado = [];
     for (var i = 0; i < linhasA; i++) {
         resultado[i] = [];
@@ -29,6 +34,7 @@ else {
         }
     }
 
+    // multiplicando A e B
     for (var i = 0; i < linhasA; i++) {
         for (var j = 0; j < colunasB; j++) {
             for (var k = 0; k < colunasA; k++) {
