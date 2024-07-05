3683979fd0f547a1882e71fc3bd9a4d385989ea9
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-07-05 16:07:46 -0300

    feat: resolvendo o exercicio extra de numero 1

---
+++ ex1-extra.js
@@ -0,0 +1,14 @@
+/* Faça um programa que leia 6 valores. Estes valores são somente
+negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a
+quantidade de valores positivos digitados. */
+
+var n1 = parseFloat(prompt("Digite um numero: "))
+var n2 = parseFloat(prompt("Digite um numero: "))
+var n3 = parseFloat(prompt("Digite um numero: "))
+var n4 = parseFloat(prompt("Digite um numero: "))
+var n5 = parseFloat(prompt("Digite um numero: "))
+var n6 = parseFloat(prompt("Digite um numero: "))
+lista = []
+lista.push(n1, n2, n3, n4, n5, n6)
+
+console.log(lista)
